7182e41efe54bbc83025496e3beebbec67c68fbe
Gervásio Artur <gervasioartur@outlook.com.br>
2026-04-07 17:49:59 +0200

    chore: improve code quality

--- _test/application/user/boostrap_sa/bootstrap_sa_test.go
+++ _test/application/user/boostrap_sa/bootstrap_sa_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 
 	"github.com/oderapi/_test/application/user/boostrap_sa/mocks"
-	"github.com/oderapi/application/user/bootstrapp_sa"
 	"github.com/oderapi/domain/gateway"
 	"github.com/oderapi/domain/shared"
+	"github.com/oderapi/usecase/user/bootstrapp_sa"
 )
 
 func TestShouldReturnNilIfSuperAdminAlreadyExists(t *testing.T) {
@@ -272,6 +272,61 @@ func TestShouldReturnErrorIfEmailEmpty(t *testing.T) {
 }
 
 func TestShouldReturnErrorIfUsernameEmpty(t *testing.T) {
+	//Arrange
+	username := ""
+	userGateway := &mocks.UserGatewayMock{ExistsActiveSuperAdminParam: username}
+	idGenerator := &mocks.IDGeneratorMock{GenerateResult: "test-id"}
+	encodeParam := "str0ngP@ssword"
+	encodedPassword := "encodedPassword"
+	encoder := &mocks.EncoderGatewayMock{EncodeResult: encodedPassword}
+	expectedError := shared.RequiredField("User", "username")
+
+	usecase := bootstrapp_sa.New(userGateway, idGenerator, encoder)
+	input := bootstrapp_sa.Input{
+		Username: username,
+		Name:     "System Admin",
+		Email:    "sa@system.com",
+		Password: encodeParam,
+	}
+
+	//Act
+	output, err := usecase.Execute(input)
+
+	//Assert
+	if err == nil {
+		t.Errorf("should  return an error")
+	}
+
+	if output != nil {
+		t.Errorf("output should be nil")
+	}
+
+	if userGateway.ExistsActiveSuperAdminResult {
+		t.Error("ExistsActiveSuperAdmin should return false")
+	}
+
+	if !userGateway.ExistsActiveSuperAdminCalled {
+		t.Error("ExistsActiveSuperAdmin should have been called")
+	}
+
+	if !idGenerator.GenerateCalled {
+		t.Error("Generate should have been called")
+	}
+
+	if !encoder.EncodeCalled {
+		t.Error("Encoder should have been called")
+	}
+
+	if err.Code != expectedError.Code {
+		t.Errorf("Should return error code: %v", expectedError.Code)
+	}
+
+	if err.Message != expectedError.Message {
+		t.Errorf("Should return error message: %v", expectedError.Message)
+	}
+}
+
+func TestShouldReturnErrorIfPasswordIsEmpty(t *testing.T) {
 	//Arrange
 	username := ""
 	userGateway := &mocks.UserGatewayMock{ExistsActiveSuperAdminParam: username}
