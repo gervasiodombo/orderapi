4a33b075669c233a1a4f8e5db9d968c99727b7a3
Gervásio Artur <gervasioartur@outlook.com.br>
2026-04-07 17:46:43 +0200

    test: ensure bootstrap_sa returns bad request if username is empty

--- _test/application/user/boostrap_sa/bootstrap_sa_test.go
+++ _test/application/user/boostrap_sa/bootstrap_sa_test.go
@@ -270,3 +270,58 @@ func TestShouldReturnErrorIfEmailEmpty(t *testing.T) {
 		t.Errorf("Should return error message: %v", expectedError.Message)
 	}
 }
+
+func TestShouldReturnErrorIfUsernameEmpty(t *testing.T) {
+	//Arrange
+	username := ""
+	userGateway := &mocks.UserGatewayMock{ExistsActiveSuperAdminParam: username}
+	idGenerator := &mocks.IDGeneratorMock{GenerateResult: "test-id"}
+	encodeParam := "str0ngP@ssword"
+	encodedPassword := "encodedPassword"
+	encoder := &mocks.EncoderGatewayMock{EncodeResult: encodedPassword}
+	expectedError := shared.RequiredField("User", "username")
+
+	usecase := bootstrapp_sa.New(userGateway, idGenerator, encoder)
+	input := bootstrapp_sa.Input{
+		Username: username,
+		Name:     "System Admin",
+		Email:    "sa@system.com",
+		Password: encodeParam,
+	}
+
+	//Act
+	output, err := usecase.Execute(input)
+
+	//Assert
+	if err == nil {
+		t.Errorf("should  return an error")
+	}
+
+	if output != nil {
+		t.Errorf("output should be nil")
+	}
+
+	if userGateway.ExistsActiveSuperAdminResult {
+		t.Error("ExistsActiveSuperAdmin should return false")
+	}
+
+	if !userGateway.ExistsActiveSuperAdminCalled {
+		t.Error("ExistsActiveSuperAdmin should have been called")
+	}
+
+	if !idGenerator.GenerateCalled {
+		t.Error("Generate should have been called")
+	}
+
+	if !encoder.EncodeCalled {
+		t.Error("Encoder should have been called")
+	}
+
+	if err.Code != expectedError.Code {
+		t.Errorf("Should return error code: %v", expectedError.Code)
+	}
+
+	if err.Message != expectedError.Message {
+		t.Errorf("Should return error message: %v", expectedError.Message)
+	}
+}
