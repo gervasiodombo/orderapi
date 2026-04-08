d9d58ca6b56e6bf69839396101fe6267a71ca124
gervasiodombo <gervasiorthur@gmil.com>
2026-04-08 21:53:10 +0200

    test: ensure user_gateway returns error if Save fails

--- infra/gateway/user_gateway_test.go
+++ infra/gateway/user_gateway_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	mocks "github.com/oderapi/_mocks"
+	"github.com/oderapi/domain/entity/user"
+	"github.com/oderapi/domain/shared"
 	"github.com/oderapi/infra/gateway"
 	"github.com/stretchr/testify/assert"
 )
@@ -33,4 +35,18 @@ func TestShouldReturnBoolExistsActiveSuperAdminSucceeds(t *testing.T) {
 	//Assert
 	assert.Nil(t, err)
 	assert.True(t, result)
+}
+
+func TestShouldReturnErrorIfSaveFails(t *testing.T) {
+	//Arrange
+	userRepository := &mocks.UserRepositoryMock{SaveErr: errors.New("db connection failed")}
+	userGateway := gateway.NewUserGatewayImpl(userRepository)
+	id, _ := shared.NewID("test-sa-id")
+	us := user.With(id, "any_name", "any_email", "any_username", "any_passq", user.ACTIVE, []user.Role{user.SUPER_ADMIN})
+
+	//Act
+	err := userGateway.Save(us)
+
+	//Assert
+	assert.NotNil(t, err)
 }
