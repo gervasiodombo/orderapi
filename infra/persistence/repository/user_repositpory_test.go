0be820c08534ca82244ff1f8251fdd6e3b922e11
gervasiodombo <gervasiorthur@gmil.com>
2026-04-09 08:55:01 +0200

    test: ensure user_repository returns true if user exists on ExistsActiveSuperAdmin

--- infra/persistence/repository/user_repositpory_test.go
+++ infra/persistence/repository/user_repositpory_test.go
@@ -3,6 +3,7 @@ package repository_test
 import (
 	"testing"
 
+	"github.com/oderapi/infra/persistence/model"
 	"github.com/oderapi/infra/persistence/repository"
 	"github.com/stretchr/testify/assert"
 )
@@ -18,4 +19,20 @@ func TestShouldReturnFalseIfUserDoesNotExistOnExistSuperAdmin(t *testing.T) {
 	//Assert
 	assert.Nil(t, err)
 	assert.False(t, exists)
+}
+
+func TestShouldReturnTrueIfUserDoesNotExistOnExistSuperAdmin(t *testing.T) {
+	//Arrange
+	db := newTestDb(t)
+	userRepository := repository.NewUserRepositoryImpl(db)
+	user := model.UserModel{ID: "uuid-1", Name: "Admin", Email: "sa@test.com", Username: "sa", Password: "hash", Status: "ACTIVE"}
+	db.Create(&user)
+	db.Create(&model.RoleModel{UserID: "uuid-1", Role: "SUPER_ADMIN"})
+
+	//Act
+	exists, err := userRepository.ExistsActiveSuperAdmin()
+
+	//Assert
+	assert.Nil(t, err)
+	assert.True(t, exists)
 }
