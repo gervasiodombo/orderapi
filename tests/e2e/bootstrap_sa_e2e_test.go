c241d2ba1647125268312acb6b57fb28a709e5c8
gervasiodombo <gervasiorthur@gmil.com>
2026-04-09 12:36:39 +0200

    test: ensure system does not create SA if already exist on startup

--- tests/e2e/bootstrap_sa_e2e_test.go
+++ tests/e2e/bootstrap_sa_e2e_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/oderapi/src/usecase/user/bootstrapp_sa"
 	"github.com/oderapi/tests/mocks"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestE2EShouldFailWhenEnvsMissing(t *testing.T) {
@@ -32,4 +33,23 @@ func TestShouldCreateSAOnFirstStartUp(t *testing.T) {
 	output, err := mocks.RunOnStart(t, db, input)
 	assert.NoError(t, err)
 	assert.Contains(t, output, "Super Admin successfully created")
+}
+
+func TestShouldNotCreateSAOnFirstStartUpIfAlreadyExists(t *testing.T) {
+	db, cleanup := mocks.StartPostgres(t)
+	defer cleanup()
+	input := bootstrapp_sa.BootstrapSAInput{
+		Name:     "System Admin",
+		Email:    "sa@system.com",
+		Username: "superadmin",
+		Password: "strOnP@ssword",
+	}
+
+	output, err := mocks.RunOnStart(t, db, input)
+	require.NoError(t, err)
+
+	output, err = mocks.RunOnStart(t, db, input)
+
+	assert.NoError(t, err)
+	assert.Contains(t, output, "Super Admin successfully already created")
 }
