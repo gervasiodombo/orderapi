756d93cacd0c1293773cf9a51786227692024a2e
gervasiodombo <gervasiorthur@gmil.com>
2026-04-10 15:09:06 +0200

    chore: improve code quality

--- src/infra/mapper/user_mapper.go
+++ src/infra/mapper/user_mapper.go
@@ -1,21 +1,21 @@
 package mapper
 
 import (
-	user2 "github.com/oderapi/src/domain/entity/user"
+	"github.com/oderapi/src/domain/entity/user"
 	"github.com/oderapi/src/domain/shared"
-	model2 "github.com/oderapi/src/infra/persistence/model"
+	"github.com/oderapi/src/infra/persistence/model"
 )
 
-func ToModel(u user2.User) model2.UserModel {
-	roles := make([]model2.RoleModel, len(u.Roles()))
+func ToModel(u user.User) model.UserModel {
+	roles := make([]model.RoleModel, len(u.Roles()))
 	for i, role := range u.Roles() {
-		roles[i] = model2.RoleModel{
+		roles[i] = model.RoleModel{
 			UserID: u.Id().Value(),
 			Role:   role.String(),
 		}
 	}
 
-	return model2.UserModel{
+	return model.UserModel{
 		ID:       u.Id().Value(),
 		Name:     u.Name(),
 		Email:    u.Email(),
@@ -26,16 +26,16 @@ func ToModel(u user2.User) model2.UserModel {
 	}
 }
 
-func ToDomain(m model2.UserModel) (user2.User, error) {
+func ToDomain(m model.UserModel) (user.User, error) {
 	id, err := shared.NewID(m.ID)
 	if err != nil {
-		return user2.User{}, err
+		return user.User{}, err
 	}
 
-	roles := make([]user2.Role, len(m.Roles))
+	roles := make([]user.Role, len(m.Roles))
 	for i, r := range m.Roles {
-		roles[i] = user2.Role(r.Role)
+		roles[i] = user.Role(r.Role)
 	}
 
-	return user2.With(id, m.Name, m.Email, m.Username, m.Password, user2.Status(m.Status), roles), nil
+	return user.With(id, m.Name, m.Email, m.Username, m.Password, user.Status(m.Status), roles), nil
 }
